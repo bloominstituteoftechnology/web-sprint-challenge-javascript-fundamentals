9c0fd55e11f46eb5ea5d78a67713d9fb44c7116a
unknown <antuan.thompson91@gmail.com>
2020-06-14 22:44:22 -0500

    factored request 1 and 3 of array-callback.js

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -18,9 +18,14 @@ const zooAnimals = [
 /* Request 1: .forEach()
 
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
-
+//
 */
 const displayNames = [];
+
+zooAnimals.forEach(function(animal) {
+displayNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`);
+
+});
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -29,7 +34,10 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(function(items){
+  return items.animal_name.toLowerCase();
+});
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
