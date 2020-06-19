9d1409d00b04a1cb094cd5a723ec3ac0e10aef88
Ashley Bergsma <ashley.bergsma42@gmail.com>
2020-06-19 08:47:04 -0700

    prototypes done

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,6 +21,7 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -29,7 +30,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = zooAnimals.map((firstNames) => {
+  return(data.animal_name);
+});
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
