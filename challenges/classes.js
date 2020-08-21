2cbc6bd023efdfadfb5162264602de9b8adaaedc
Prince Buduru <princebuduru1995@gmail.com>
2020-08-21 12:43:04 -0400

    im sorry i forgot to commit frequently but its done :)

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,39 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
+// function CuboidMaker(attrs){
+//   this.length = attrs.length;
+//   this.width = attrs.width;
+//   this.height = attrs.height;
+// }
+// CuboidMaker.prototype.volume = function(){
+//   return (this.length*this.width*this.height);
+// }
+// CuboidMaker.prototype.surfaceArea = function(){
+//   return (this.length*this.width) + (this.length*this.height) + (this.width*this.height)
+// }
+// const cuboid = new CuboidMaker({
+//   length: 4,
+//   width: 5,
+//   height: 5,
+// });
+//  class syntax
+
+class CuboidMaker{
+  constructor(attrs){
+    this.length = attrs.length;
+    this.width = attrs.width;
+    this.height = attrs.height;
+  }
+}
+
+function volume(){
+  return (this.length*this.width*this.height);
+}
+function surfaceArea(){
+  return (this.length*this.width) + (this.length*this.height) + (this.width*this.height)
+}
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
--- challenges/closure.js
+++ challenges/closure.js
@@ -3,22 +3,32 @@
 /* Task 1: Study the code below and explain in your own words why nested function can access the variable internal. */
 
 
-const external = "I'm outside the function";
+// const external = "I'm outside the function";
 
-function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+// function myFunction() {
+//   console.log(external);
+//   const internal = "Hello! I'm inside myFunction!";
 
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
-}
-myFunction();
+//   function nestedFunction() {
+//     console.log(internal);
+//   };
+//   nestedFunction();
+// }
+// myFunction();
 
-// Explanation: 
+// Explanation: because a closure gives you access to an outer function's scope from an inner function and int this case 'external' is globally declared.
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(n){
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+summation(4);
