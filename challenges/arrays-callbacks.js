78ecf6a55cd27b8445dc8c252b21ed7a0aec304e
QDG <nasha@QDGs-MacBook-Air.local>
2020-08-21 09:01:55 -0700

    finshed sprint and stretch

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,7 +21,8 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
-zooAnimals.forEach(aminal => displayNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`));
+zooAnimals.forEach(function(animal){
+  displayNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`)});
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -31,17 +32,18 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowerCase = [];
-zooAnimals.map(zooAnimal => lowerCase.push(zooAnimal.animal_name.toLowerCase()));
-
+zooAnimals.map(function(zooAnimal){
+  lowerCase.push(zooAnimal.animal_name.toLowerCase())});
 console.log(lowerCase); 
+
 
 /* Request 3: .filter() 
 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = zooAnimals.filter(
-  zooAnimal => zooAnimal.population < 5);
+const lowPopulationAnimals = zooAnimals.filter(function(zooAnimal){
+  zooAnimal.population < 5});
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -49,8 +51,10 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
-console.log(populationTotal);
+const populationTotal = zooAnimals.reduce(function(acc,val){
+  return acc + val.population;
+  },0);
+ console.log(populationTotal);
 
 
 // ==== Callbacks ====  
@@ -61,19 +65,32 @@ console.log(populationTotal);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+let consume = function(a, b, cb) {
+  return cb(a,b);
+ }
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add (a,b) {
+  return a + b;
+}
 
+function multiply (a,b) {
+  return a * b;
+}
+
+
+function greeting (first,last) {
+  return `Hello ${first} ${last}, nice to meet you!`;
+};
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply)); // 160
+ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
@@ -83,4 +100,25 @@ console.log(populationTotal);
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
 */
+const displayNames2 = [];
+zooAnimals.forEach(animal => displayNames2.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`));
+console.log(displayNames2);
 
+
+
+const lowerCase2 = [];
+zooAnimals.map(zooAnimal => lowerCase2.push(zooAnimal.animal_name.toLowerCase()));
+console.log(lowerCase2); 
+
+
+
+const lowPopulationAnimals2 = zooAnimals.filter(
+  zooAnimal => zooAnimal.population < 5);
+console.log(lowPopulationAnimals2);
+
+
+
+const populationTotal2 = zooAnimals.reduce((acc,val) => {
+  return acc + val.population;
+ },0);
+ console.log(populationTotal2);
--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,24 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+//It's a nested function so it can access variables from the first function.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(number) {
+  // use a counter
+  let summation = 0
+  
+  for(let i = 1; i <= number; i++){
+   console.log(i)
+   //console.log(i+1)
+   summation = summation + i
+  //  summation = 0 + 1
+  //  summation = 1 + 2
+  //  summation = 2 + 3
+    //console.log(summation)
+  }
+  return summation
+}
+console.log(summation(4))
