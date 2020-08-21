432e3aeedc830fda30e82317c1769885ae98f288
Ashley Slater <orecodev@gmail.com>
2020-08-21 08:14:11 -0700

    Finished closures

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,17 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+Because we are console.log'ing it inside the function and then initializing it after, but we have not ended/initalized the parent function yet. 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(summation(8));
