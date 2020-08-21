b7ca5010fab88a37523f08bf6a8f2b420af6f43d
Simone Ballard <simone.a.ballard@gmail.com>
2020-08-21 11:43:26 -0600

    unknown error in running the class

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -19,7 +19,7 @@ function CuboidMaker (length, width, height) {
   Formula for cuboid volume: length * width * height
 */
 CuboidMaker.prototype.volume = function() {
-  return `${this.length} * ${this.width} * ${this.height}`;
+  return `${this.length * this.width * this.height}`;
 }
 
 /* == Step 3: Surface Area Method ==
@@ -29,7 +29,7 @@ CuboidMaker.prototype.volume = function() {
 */
 
 CuboidMaker.prototype.surfacearea = function() {
-  return `(2 * (${this.length} * ${this.width} + ${this.length} * ${this.height} + ${this.width} * ${this.height}))`;
+  return `${2 * (this.length * this.width + this.length * this.height + this.width * this.height)}`;
 }
 
 
@@ -39,9 +39,9 @@ CuboidMaker.prototype.surfacearea = function() {
 */
 
 const cuboid = new CuboidMaker({
-  length: '4',
-  width: '5',
-  height: '5',
+  length: 4,
+  width: 5,
+  height: 5,
 });
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
