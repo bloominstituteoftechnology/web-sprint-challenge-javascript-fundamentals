16a86e980b526fcb96bad4cc1a8ceebab5ad2b3a
Jonah D. Hadlock <jonah-hadlock@lambdastudents.com>
2020-09-11 20:12:21 -0600

    working on ===callbacks===

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -78,19 +78,18 @@ function consume(a, b, cb) {
  * Create a function named multiply that returns the product of two numbers 
  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
  */
-function add(num1, num2) {
+add(num1, num2) {
     return num1 + num2;
 }
 
-function multiply(num1, num2) {
+multiply(num1, num2) {
     return num1 * num2;
 }
 
-function greeting() {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    return `Hello ${this.firstName} ${this.lastName}, nice to meet you!`;
+greeting() {
+    return `Hello ${first-name} ${last-name}, nice to meet you!`;
 }
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 console.log(consume(2, 2, add)); // 4
