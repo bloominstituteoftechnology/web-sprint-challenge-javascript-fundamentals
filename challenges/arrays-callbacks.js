6c282a36a71867db2a71865eb9b69762044ce848
Jonah D. Hadlock <jonah-hadlock@lambdastudents.com>
2020-09-11 20:01:07 -0600

    working on the last stuff in callbacks

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -1,65 +1,65 @@
-// ==== ADVANCED Array Methods ====
+// // ==== ADVANCED Array Methods ====
 
-// Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
+// // Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
 
-const zooAnimals = [
-    { animal_name: "Jackal, asiatic", population: 5, scientific_name: "Canis aureus", state: "Kentucky" },
-    { animal_name: "Screamer, southern", population: 1, scientific_name: "Chauna torquata", state: "Alabama" },
-    { animal_name: "White spoonbill", population: 8, scientific_name: "Platalea leucordia", state: "Georgia" },
-    { animal_name: "White-cheeked pintail", population: 1, scientific_name: "Anas bahamensis", state: "Oregon" },
-    { animal_name: "Black-backed jackal", population: 2, scientific_name: "Canis mesomelas", state: "Washington" },
-    { animal_name: "Brolga crane", population: 9, scientific_name: "Grus rubicundus", state: "New Mexico" },
-    { animal_name: "Common melba finch", population: 5, scientific_name: "Pytilia melba", state: "Pennsylvania" },
-    { animal_name: "Pampa gray fox", population: 10, scientific_name: "Pseudalopex gymnocercus", state: "Connecticut" },
-    { animal_name: "Hawk-eagle, crowned", population: 10, scientific_name: "Spizaetus coronatus", state: "Florida" },
-    { animal_name: "Australian pelican", population: 5, scientific_name: "Pelecanus conspicillatus", state: "West Virginia" },
-];
+// const zooAnimals = [
+//     { animal_name: "Jackal, asiatic", population: 5, scientific_name: "Canis aureus", state: "Kentucky" },
+//     { animal_name: "Screamer, southern", population: 1, scientific_name: "Chauna torquata", state: "Alabama" },
+//     { animal_name: "White spoonbill", population: 8, scientific_name: "Platalea leucordia", state: "Georgia" },
+//     { animal_name: "White-cheeked pintail", population: 1, scientific_name: "Anas bahamensis", state: "Oregon" },
+//     { animal_name: "Black-backed jackal", population: 2, scientific_name: "Canis mesomelas", state: "Washington" },
+//     { animal_name: "Brolga crane", population: 9, scientific_name: "Grus rubicundus", state: "New Mexico" },
+//     { animal_name: "Common melba finch", population: 5, scientific_name: "Pytilia melba", state: "Pennsylvania" },
+//     { animal_name: "Pampa gray fox", population: 10, scientific_name: "Pseudalopex gymnocercus", state: "Connecticut" },
+//     { animal_name: "Hawk-eagle, crowned", population: 10, scientific_name: "Spizaetus coronatus", state: "Florida" },
+//     { animal_name: "Australian pelican", population: 5, scientific_name: "Pelecanus conspicillatus", state: "West Virginia" },
+// ];
 
-/* Request 1: .forEach()
+// /* Request 1: .forEach()
 
-The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
+// The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
-*/
-const displayNames = [];
-const animalNames = (name) => {
-    displayNames.push(`Name: ${name.animal_name}, Scientific: ${name.scientific_name}`);
-}
+// */
+// const displayNames = [];
+// const animalNames = (name) => {
+//     displayNames.push(`Name: ${name.animal_name}, Scientific: ${name.scientific_name}`);
+// }
 
-zooAnimals.forEach(animalNames);
-console.log(displayNames);
+// zooAnimals.forEach(animalNames);
+// console.log(displayNames);
 
-/* Request 2: .map()
+// /* Request 2: .map()
 
-The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
+// The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 
-*/
+// */
 
-const lowCaseAnimalNames = [];
-const lowCaseNames = (names) => {
-    lowCaseAnimalNames.push(`${names.animal_name.toLowerCase()}`);
-}
-zooAnimals.map(lowCaseNames);
-console.log(lowCaseAnimalNames);
+// const lowCaseAnimalNames = [];
+// const lowCaseNames = (names) => {
+//     lowCaseAnimalNames.push(`${names.animal_name.toLowerCase()}`);
+// }
+// zooAnimals.map(lowCaseNames);
+// console.log(lowCaseAnimalNames);
 
-/* Request 3: .filter() 
+// /* Request 3: .filter() 
 
-The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
+// The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
-*/
-const lowPopulationAnimals = zooAnimals.filter(zooAnimals => zooAnimals.population < 5);
+// */
+// const lowPopulationAnimals = zooAnimals.filter(zooAnimals => zooAnimals.population < 5);
 
-console.log(lowPopulationAnimals);
+// console.log(lowPopulationAnimals);
 
-/* Request 4: .reduce() 
+// /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+// The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
-*/
-let populationTotal = 0;
+// */
+// let populationTotal = 0;
 
-const animalNumbers = (animal, number) => animal + number.population;
+// const animalNumbers = (animal, number) => animal + number.population;
 
-console.log(zooAnimals.reduce(animalNumbers, populationTotal));
+// console.log(zooAnimals.reduce(animalNumbers, populationTotal));
 
 // ==== Callbacks ====  
 
@@ -78,16 +78,21 @@ function consume(a, b, cb) {
  * Create a function named multiply that returns the product of two numbers 
  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
  */
+function add(num1, num2) {
+    return num1 + num2;
+}
 
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function greeting() {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    return `Hello ${this.firstName} ${this.lastName}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
-
-
-/*
-
-Stretch: If you haven't already, convert your array method callbacks into arrow functions.
-
-*/
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
