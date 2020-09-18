d00ea8db104b7d4f5e0e4cecd9ae02a15bcfa218
bria barry <briabarry@imac.lan>
2020-09-18 10:58:31 -0500

    Finishing last couple errors

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -15,64 +15,76 @@ const zooAnimals = [
   { animal_name: "Australian pelican", population: 5, scientific_name: "Pelecanus conspicillatus", state: "West Virginia" },
 ];
 
-/* Request 1: .forEach()
-
-The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
-
-*/
+// /* Request 1: .forEach() 
 const displayNames = [];
+
+zooAnimals.forEach(function(item,){
+    displayNames.push(item.animal_name + item.scientific_name);
+});
+
 console.log(displayNames);
 
 /* Request 2: .map()
-
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
-
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = zooAnimals.map(function(item){
+  return item.animal_name.toLowerCase();
+});
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
-
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
-
 */
-const lowPopulationAnimals
+const lowPopulationAnimals = zooAnimals.filter(function(item){
+  return item.population <= 5;
+});
+
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
-
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
-
 */
-let populationTotal = 0;
+let populationTotal = zooAnimals.reduce(function(accum, item){
+  return accum + item.population;
+}, 0)
+
 console.log(populationTotal);
 
 
 // ==== Callbacks ====  
 
 /* Step 1: Create a higher-order function
-  * Create a higher-order function named consume with 3 parameters: a, b and cb
-  * The first two parameters can take any argument (we can pass any value as argument)
-  * The last parameter accepts a callback
-  * The consume function should return the invocation of cb, passing a and b into cb as arguments
+* Create a higher-order function named consume with 3 parameters: a, b and cb
+* The first two parameters can take any argument (we can pass any value as argument)
+* The last parameter accepts a callback
+* The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(a, b, callback){
+  return callback(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+* Create a function named add that returns the sum of two numbers
+* Create a function named multiply that returns the product of two numbers 
+* Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(c, d){
+  return c + d;
+}
 
+function multiply (c, d){
+  return c * d;
+}
+
+function greeting (fname, lname){
+  return `Hello ${fname}${lname}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
-
-
-
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 /*
 
--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,17 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+// 'nestedFunction' can access the variable 'internal' because of the lexical scope. Lexical means that we can pass information down into another function. 'nestedFunction' is inside of 'myFunction' where the 'internal' variable was created. Thus giving the 'nestedFunction' access to everything within 'myFunction'.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+var summation = (function () {
+  var counter = 0;
+  return function () {counter += 1; return counter}
+})();
+
+console.log(summation());
+console.log(summation());
+console.log(summation());
