a88d90d8f71806ad447e955e63f1ab352d90fe25
Eduardo Maldonado <bones9750@gmail.com>
2020-09-18 15:37:39 -0700

    was able to finish Array-callbacks but might need to review it more

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -44,8 +44,8 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = lowPopulationAnimals.filter(function(item){
-  return
+const lowPopulationAnimals = zooAnimals.filter(function(item){
+  return item.population <= 5;
 })
 console.log(lowPopulationAnimals);
 
@@ -56,8 +56,8 @@ The zoos need to know their total animal population across the United States. Fi
 */
 let populationTotal = 0;
 
-const population = newPopulation.reduce(function(accumulator, item){
-  return
+const population = zooAnimals.reduce(function(accumulator, item){
+  return accumulator + item.population
 }, 0)
 console.log(populationTotal);
 
@@ -71,6 +71,9 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a, b, cb){
+  return cb(a, b)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -78,11 +81,22 @@ console.log(populationTotal);
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a, b, cb){
+  return cb(a + b);
+}
+
+function multiply(a, b, cb){
+  return cb(a * b);
+}
+
+function greeting(a, b, cb){
+  return cb(`Hello ${a} ${b}, nice to meet you!`);
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
