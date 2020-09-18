ef10de58bef1d3883780dc4ae88e0cbc58d1955c
Sam Singer <sam.singet79@gmail.com>
2020-09-18 08:56:31 -0700

    sam-singer pt2 done

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,25 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: The reason that nested function can inherit the variable internal is because it is nested inside of a parent function which contains that variable name, hence a child can always inherit from its parent but not the other way around. So its really as simple as that when calling the function it first checks its local memoy and when it doesnt find it there it look up to its parent in which case the parent has it, other wise it would continue on to global memory and try to find it there.
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+ function summation(param){
+  function countingArr(){counter = 0
+  counterArray = []
+  for(i=0; i <= param; i++){
+     
+     counterArray.push(counter ++)
+  }
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  
+  return counterArray.reduce(reducer)
+  }
+
+ 
+  return countingArr()
+}
+console.log(summation(6))
