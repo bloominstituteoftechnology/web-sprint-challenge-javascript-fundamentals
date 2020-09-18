ae3f2af2293a1a8f600e9eba823dbdaae2470cbe
Jennifer Kramer <jenkrame@gmail.com>
2020-09-18 13:03:34 -0400

    Completed Step 1 on arrays-callbacks.js file.

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -68,6 +68,10 @@ console.log(zooAnimals.reduce(reducer, 0));
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume (a, b, cb){
+  return cb(a,b)
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
