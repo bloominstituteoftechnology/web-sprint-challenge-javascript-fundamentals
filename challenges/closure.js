8894db7f4a2e4b8dee5d7d51d35e59986b45505f
Eduardo Maldonado <bones9750@gmail.com>
2020-09-18 09:28:51 -0700

    I think so far its on the right track

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,15 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: It can be accessed since it in dealing with scopes. since the variable is within a function it can only be accessed from within, by calling out the function, it can overwrite the global into local
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+
+
+const summation = sum.reduce(callbackfunction(add, state) => {
+  return add += state.number;
+}, 0)
