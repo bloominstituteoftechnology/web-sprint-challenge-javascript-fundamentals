20d7639684d33dd2ce999cbe37a8e404e68e388b
Sam Singer <sam.singet79@gmail.com>
2020-09-18 08:36:46 -0700

    sam-singer Finished Sprint

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -20,63 +20,89 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
-console.log(displayNames);
+// const displayNames = [];
+// zooAnimals.forEach(function(items){
+//   let animalsName = `Name: ${items.animal_name}`
+  
+//   let scientificName = `Scientific name: ${items.scientific_name}`
+//   displayNames.push(animalsName,scientificName)
+// })
+// console.log(displayNames);
 
-/* Request 2: .map()
+// /* Request 2: .map()
 
-The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
+// The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 
-*/
+// */
 
-const lowCaseAnimalNames
-console.log(lowCaseAnimalNames);
+// const lowCaseAnimalNames = zooAnimals.map(function(itemsLow){
+//  return itemsLow.animal_name.toLowerCase()
+// })
+// console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals
-console.log(lowPopulationAnimals);
 
-/* Request 4: .reduce() 
+// const lowPopulationAnimals = zooAnimals.filter(lowPop => lowPop.population < 5)
+// console.log(lowPopulationAnimals);
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+// /* Request 4: .reduce() 
 
-*/
-let populationTotal = 0;
-console.log(populationTotal);
+// The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
-
-// ==== Callbacks ====  
-
-/* Step 1: Create a higher-order function
-  * Create a higher-order function named consume with 3 parameters: a, b and cb
-  * The first two parameters can take any argument (we can pass any value as argument)
-  * The last parameter accepts a callback
-  * The consume function should return the invocation of cb, passing a and b into cb as arguments
-*/
+// */
 
 
-/* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+// const populationArray = []
+// zooAnimals.forEach(pops => populationArray.push(pops.population))
+// console.log(populationArray)
+// const reducer = (accumulator,currentValue) => accumulator + currentValue;
+// console.log(populationArray.reduce(reducer, 0))
 
 
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+// // ==== Callbacks ====  
+
+// /* Step 1: Create a higher-order function
+//   * Create a higher-order function named consume with 3 parameters: a, b and cb
+//   * The first two parameters can take any argument (we can pass any value as argument)
+//   * The last parameter accepts a callback
+//   * The consume function should return the invocation of cb, passing a and b into cb as arguments
+// */
+function consume(a,b,cb){
+  return cb(a,b)
+
+}
+
+// /* Step 2: Create several functions to callback with consume();
+//   * Create a function named add that returns the sum of two numbers
+//   * Create a function named multiply that returns the product of two numbers 
+//   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+// */
+const add = function add(a,b){
+  return a + b
+}
+const multiply = function multiply(a,b){
+  return a * b
+}
+const greeting = function greeting(first,last){
+  return `Hello ${first} ${last}, nice to meet you! `
+}
+
+
+// /* Step 3: Check your work by un-commenting the following calls to consume(): */
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
 
-/*
+// /*
 
-Stretch: If you haven't already, convert your array method callbacks into arrow functions.
+// Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
-*/
+// */
 
