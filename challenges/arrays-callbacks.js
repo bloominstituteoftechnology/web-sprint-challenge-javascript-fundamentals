a189294af5a58925669f05da34ab0136fc309928
jolness1 <jolness1@gmail.com>
2020-10-16 11:17:23 -0600

    Classes, Prototypes and closures Done; arrays/callbacks partially done

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,7 +21,12 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];{
-  zooAnimals.forEach()
+ zooAnimals.forEach((animal)=> {
+  displayNames.push(
+    `Name: ${animal.animal_name} Scientific: ${animal.scientific_name}`
+  )
+ });
+  
 }
 console.log(displayNames);
 
@@ -31,25 +36,33 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = zooAnimals.map((animal)=> {
+  return animal.animal_name.toLowerCase();
+});
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
-*/
-const lowPopulationAnimals
+// */
+const lowPopulationAnimals = zooAnimals.filter((animal)=> {
+  return animal.population <= 5;
+})
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count. */
 
-*/
-let populationTotal = 0;
-console.log(populationTotal);
-
+function zooPop(callback) {
+  let populationTotal = callback.reduce((sum, item) => {
+    return sum + item["population"];
+  }, 0);
+  let popTotal = populationTotal;
+  return popTotal;
+}
+console.log(zooPop(zooAnimals));
 
 // ==== Callbacks ====  
 
