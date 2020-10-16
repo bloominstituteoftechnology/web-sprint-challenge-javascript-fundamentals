f4500e4831ce16f0390d8d7a49cc5994779313ee
Caudill-Z <71600918+Caudill-Z@users.noreply.github.com>
2020-10-16 11:02:46 -0500

    callbacks section completed

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -65,14 +65,26 @@ console.log(populationTotal);
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function consume(a, b, cb){
+  return cb(a,b);
+}
+
+function add(num1, num2){
+  return num1 += num2;
+}
+
+function multiply(num1, num2){
+ return num1 *= num2;
+}
+
+function greeting(firstName, lastName){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
-
-
-
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 /*
 
