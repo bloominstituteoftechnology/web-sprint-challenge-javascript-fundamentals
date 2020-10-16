a97cfe116f048c10c1fe4856e7e401ed5e5d12d9
jolness1 <jolness1@gmail.com>
2020-10-16 10:21:52 -0600

    Classes, Prototypes and closures Done

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,18 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+  // They are within the same scope. Can look outward for the variable, just can't look into a nested function.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+
+function summation(num){
+  let counter = 0;
+  for(let i = 1; i<=num; i++){
+    counter += i;
+  }
+  return counter;
+}
+  console.log(summation(4));
