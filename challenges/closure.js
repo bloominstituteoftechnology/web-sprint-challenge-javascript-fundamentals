2bce65b77d204134dc078b7a4115f6496f2a27b1
Molly Brown <mollybrown0516@gmail.com>
2020-11-06 22:43:59 -0500

    completed closure.js

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,19 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+   // This is due to what is called 'closure'. A closure exists when there is a function present, where a variable inside the function needs to reach outside of the function to get information. When the inner scope references variables in the outer scope --> that is what is referred to as a closure, and allows for that information to be obtained.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(number){
+  let counter = 0;
+  for (let i = 1; i <= number; i++){
+    counter += i;
+  }
+  return counter;
+}
+
+console.log(summation(4));
+
