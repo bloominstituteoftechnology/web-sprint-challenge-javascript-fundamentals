6e560cb03fefbd4dd3e666e08bfd6b184013c505
Laiken Frost <frostlaiken@gmail.com>
2020-11-08 16:30:12 -0500

    sprint

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,17 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: the scope of nestedFuction is inside of myFunction so it can be accessed. 
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(value){
+  var sumval =0;
+  for (let i = 1; i <= value; i++){sumval += i;
+  }
+  
+  return sumval;}
+console.log(summation(3));
