a37e02d2b1de6523720901738ae307d190b7971a
daniel-vazquez <da.vazquez728@gmail.com>
2020-11-07 19:51:51 -0700

    task 2, 3, and 4 complete

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -24,6 +24,7 @@ The zoos want to display both the scientific name and the animal name in front o
 */
 
 const displayNames = [];
+
 zooAnimals.forEach((el) => displayNames.push(`Name: ${el.animal_name}, Scientific: ${el.scientific_name}`));
 
 
@@ -36,7 +37,10 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = zooAnimals.map(function(item){
+  return item.animal_name.toLowerCase();
+});
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -44,8 +48,14 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-//const lowPopulationAnimals
-//console.log(lowPopulationAnimals);
+const lowPopulationAnimals = zooAnimals.filter(function(item){
+
+  return item.population < 5;
+
+});
+
+
+console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
@@ -53,7 +63,13 @@ The zoos need to know their total animal population across the United States. Fi
 
 */
 //let populationTotal = 0;
-//console.log(populationTotal);
+
+let populationTotal = zooAnimals.reduce(function(accumulator, item){
+  return accumulator + item.population;
+
+},0);
+
+console.log(populationTotal);
 
 
 // ==== Callbacks ====  
@@ -65,6 +81,13 @@ The zoos need to know their total animal population across the United States. Fi
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume (a, b, cb){
+
+  return cb(a, b);
+
+}
+
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -72,13 +95,25 @@ The zoos need to know their total animal population across the United States. Fi
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add (a, b){
+  return a + b;
+}
+
+function multiply (a, b){
+  return a * b;
+}
+
+function greeting (a, b){
+  return `Hello ${a} ${b}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
+//^^ all calls above are working properly per console
 
 
 /*
