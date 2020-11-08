9b3c24bfc54981a57b69b11c630730096735e219
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2020-11-08 13:48:17 -0700

    I was able to complete the challenges on the closure.js file!

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,20 @@ myFunction();
 
 // Explanation: 
 
+  // A: The nested function is able to be the variable because of closure. This allows for the function to reach within its scope and grab the variable. 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+  function summation(num)
+  {
+    let counter = 0;
+    
+    for(let i = 0; i <= 0; i++)
+    {
+      counter += i;
+      console.log(i);
+    }
+    return counter;
+  }
