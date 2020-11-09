aea186564f04f67998663442f28c0f8fb58f9e7a
samuel singletary <codysingletary92@gmail.com>
2020-11-08 23:19:21 -0500

    finished with prototypes. Not sure if i comitted the one on closures, but that one is done as well.

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,20 @@ myFunction();
 
 // Explanation: 
 
+// ***nestedFunction can access the variable internal because functions can go outside the scope just one step when searching for variables outside of itself. This is called a closure.
+
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+
+function summation(a){
+  numbers = [];
+
+  for (i = 0; i < a; i++){
+    numbers.push(i+1)
+  }
+
+  return numbers.reduce( (tot, num) => tot += num );
+}
