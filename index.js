214383899ed826a7a26ce247f6765970af52f713
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2020-12-18 14:14:17 -0500

    Tim Sprint Finish

--- index.js
+++ index.js
@@ -16,17 +16,25 @@ function myFunction() {
 myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
-
-
+// myfunction gets ran  and when so it runs a console of the const "external" 
+// then the next line of code to be ran is the "nestedFunction" which is notin the global scope but in the same scope as its parent function
 
 
 
 /* 🚀🚀🚀 Task 2: Counter 🚀🚀🚀 */
-/* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
+/* Create a function called `summation` that 
+accepts a parameter and uses a counter to 
+return the summation of that number. 
+For example, `summation(4)` should return 10 because 1+2+3+4 is 10. 
+Note, you may use a for loop for this function if you wish */
+function summation(num) {
+let newNum =  0;
+for(let i =  1;i < num +1 ;i++){
+  newNum += i;
+}
+return newNum
+}
 
-  }
  
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
@@ -46,34 +54,63 @@ const zooAnimals = [
   ];
   
   /* 🦁🦁🦁 Request 1: .forEach() 🦁🦁🦁
-  The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
+  The zoos want to display both the scientific name and the animal name in front of the habitats. 
+  Populate the displayNames array with only 
+  the animal_name and scientific_name of each animal. 
+  displayNames will be an array of strings, and each string should follow this pattern: 
+  "Name: Jackal, asiatic, Scientific: Canis aureus."
   */
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  const displayNames = [];
+
+  function animalNames(array1){
+    array1.forEach(function(item){
+      displayNames.push(`name: ${item.animal_name}, scientific: ${item.scientific_name}`)
+    });
+    return displayNames;
   }
   
 
   /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
   The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
   */
-  function lowerCaseNames(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
-  
+ const lowCaseAnimalNames = []
+
+ function lowerCaseNames(array){
+    array.map(function(item){
+      lowCaseAnimalNames.push(item.animal_name.toLowerCase())
+   });
+  return lowCaseAnimalNames;
+ }
   
   /* 🦁🦁🦁 Request 3: .filter() 🦁🦁🦁
-  The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
-  */
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
+  The zoos are concerned about animals with a lower population count. Using filter, 
+  create a new array of objects called lowPopulationAnimals 
+  which contains only the animals with a population less than 5.
+*/
+
+  const lowPopulationAni = [];
+  function lowPopulationAnimals(array3){
+    zooAnimals.filter(function(item){
+      if(item.population <= 5){
+        lowPopulationAni.push(item)
+      }
+    })
+    return lowPopulationAni
   }
   
 
   /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
-  The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+  The zoos need to know their total animal population across the United States. 
+  Find the total population from all the zoos using the .reduce() method. 
+Remember the reduce method takes two arguments: a callback (which itself takes two args),
+ and an initial value for the count.
   */
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
+  function USApop(array){
+    const totalPop= array.reduce(function(counter,item){
+      return counter + item.population
+    },0)
+    return totalPop;
+    
   }
   
   
@@ -84,24 +121,24 @@ const zooAnimals = [
     * The last parameter accepts a callback
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
-  function consume(/*Your Code Here */){
-    /*Your Code Here */
+  function consume(a, b, cb){
+    return cb(a ,b)
   }
  
   
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Create a function named add that returns the sum of two numbers 🦁🦁🦁
-  function add(/*Your Code Here */){
-    /*Your Code Here*/
+  function add(a,b){
+    return a + b;
   }
 // 🦁🦁🦁 Create a function named multiply that returns the product of two numbers 🦁🦁🦁
-  function multiply(/*Your Code Here */){
-   /*Your Code Here */
+  function multiply(a,b){
+   return a * b;
   }
 
  // 🦁🦁🦁 Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!" 🦁🦁🦁
-  function greeting(/*Your Code Here */){
-   return /*Your Code Here */
+  function greeting(first, last){
+   return `Hello ${first} ${last}, nice to meet you!`
   }
   
   // 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
@@ -118,12 +155,17 @@ const zooAnimals = [
 
 
 // 🐴🐴🐴 Topic 3: Prototypes 🐴🐴🐴 //
-//🐴🐴🐴 Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge. 🐴🐴🐴
+//🐴🐴🐴 Task: You are to build a cuboid maker that can return values
+//  for a cuboid's volume or surface area. 
+// Cuboids are similar to cubes but do not have even sides. 
+// Follow the steps in order to accomplish this challenge. 🐴🐴🐴
 /* 🐴🐴🐴 Step 1: Base Constructor 🐴🐴🐴
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height which can be initialized as an object
 */
-function CuboidMaker(/*Your Code Here */){
-  /*Your Code Here */
+function CuboidMaker(obj){
+  this.length = obj.length;
+  this.width = obj.width;
+  this.height = obj.height;
 }
 
 
@@ -131,7 +173,9 @@ function CuboidMaker(/*Your Code Here */){
   Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   Formula for cuboid volume: length * width * height   */
 
-
+CuboidMaker.prototype.volume = function(){
+  return this.length * this.width * this.height
+}
 
 
 
@@ -140,7 +184,9 @@ function CuboidMaker(/*Your Code Here */){
   Formula for cuboid surface area of a cube: 
   2 * (length * width + length * height + width * height)  */
 
-
+CuboidMaker.prototype.surfaceArea = function(){
+  return (2 * (this.length * this.width)) + (2 *(this.length * this.height)) + (2*(this.width * this.height))
+}
 
 
 
@@ -148,8 +194,8 @@ function CuboidMaker(/*Your Code Here */){
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid. */
 
-
-
+  let cuboid = new CuboidMaker(4, 5, 5);
+  
 
 
 // 🐴🐴🐴 Test your volume and surfaceArea methods by uncommenting the logs below: 🐴🐴🐴
@@ -159,11 +205,21 @@ function CuboidMaker(/*Your Code Here */){
  
 
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
-//🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
+//🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. 
+// Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
 class CuboidMakerTwo{
-
+  constructor(obj){
+    this.length = obj.length
+    this.width = obj.width
+    this.height = obj.height
+} 
+volume(){
+  return this.length * this.width * this.height;
 }
-
+surfaceArea(){
+  return (2 * (this.length * this.width)) + (2 *(this.length * this.height)) + (2*(this.width * this.height))
+}
+}
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
 // console.log(cuboidTwo.volume()); // 100
