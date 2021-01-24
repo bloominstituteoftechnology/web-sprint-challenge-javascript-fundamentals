e388e7ef69c056d118acb43c85f1e20ca90815d1
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-01-24 16:53:47 -0600

    Struggling a bit with .map and forEach, however i think i am getting the hang of it.

--- index.js
+++ index.js
@@ -30,12 +30,12 @@ For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you ma
 
 function summation(num){
   let counter = 0
-  for (let i = 1; i <= num; i++){
-    console.log(counter += i + 1)
+  for (let i = 0; i < num; i++){
+    counter += i + 1;
   }
   return counter
 }
-summation(4)
+console.log(summation(4))
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
@@ -59,10 +59,15 @@ const zooAnimals = [
   displayNames will be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function animalNames(arr){
+    let nameArray = [];
+    arr.forEach((item, item2)=>{
+      nameArray.push(`name: ${arr[item2].animal_name}, scientific: ${arr[item2].scientific_name}`)
+    })
+    return nameArray;
   }
-  
+    
+  console.log(animalNames(zooAnimals))
 
   /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
   The zoo needs a list of all their animal's names converted to lower case. 
@@ -70,9 +75,13 @@ const zooAnimals = [
   For example: ['jackal, asiatic', .....]
   */
 
-  function lowerCaseNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowerCaseNames(arr){
+    let caseSensitive = arr.map((item, item2)=>{
+      return arr[item2].animal_name.toLowerCase();
+    })
+    return caseSensitive;
   }
+  console.log(lowerCaseNames(zooAnimals))
   
   
   /* 🦁🦁🦁 Request 3: .filter() 🦁🦁🦁
