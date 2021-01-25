cd6fffb705efd13bc4b36b73e7d8fd11c09585f7
yonathan Admasu <admyoni4@yahoo.com>
2021-01-24 23:47:25 -0800

    worked on sprint-challenge and finshed

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+//For nestedfunction the variable internal is outside the function so it is like global scope.
 
 
 
@@ -28,10 +28,12 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(num) {
+  while(num>=0){
+    return num + num*(num-1)/2;
   }
+ }
+ console.log(summation(7));
  
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
@@ -56,9 +58,16 @@ const zooAnimals = [
   displayNames will be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+ function animalNames(zooAnimals){
+let index = zooAnimals.indexOf();
+let nameArr = zooAnimals.forEach(zooAnimals =>zooAnimals[index].animal_name);
+let scientificArr = zooAnimals.forEach(zooAnimals =>zooAnimals[index].scientific_name);
+let stringArr = `name:${nameArr}, scientific:${scientificArr}`;
+return stringArr;
+}
+console.log(animalNames(zooAnimals));
+
+
   
 
   /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
@@ -67,9 +76,14 @@ const zooAnimals = [
   For example: ['jackal, asiatic', .....]
   */
 
-  function lowerCaseNames(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+ function lowerCaseNames(zooAnimals){
+  let i = zooAnimals.indexOf();
+  const newArray = zooAnimals.map(item =>item.animal_name) 
+  return newArray.map(f => {
+    return f.toLowerCase();
+  });
+}
+console.log(lowerCaseNames(zooAnimals));
   
   
   /* 🦁🦁🦁 Request 3: .filter() 🦁🦁🦁
@@ -77,9 +91,11 @@ const zooAnimals = [
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+ function lowPopulationAnimals(data){
+  let lowPop = data.filter(data => data.population < 5);
+  return lowPop;
+}
+console.log(lowPopulationAnimals(zooAnimals));
   
 
   /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
@@ -88,9 +104,13 @@ const zooAnimals = [
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+ function USApop(data){
+  let total = data.reduce((acc,item) =>{
+    return acc += item.population;
+  },0);
+  return total;
+}
+console.log(USApop(zooAnimals));
   
   
   // 🦁🦁🦁 Callbacks 🦁🦁🦁  
@@ -101,29 +121,33 @@ const zooAnimals = [
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
 
-  function consume(/*Your Code Here */){
-    /*Your Code Here */
-  }
+ function consume(a,b,cb){
+  return cb(a,b);
+}
+console.log(consume(7,5,multiply));
  
   
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Use add to return the sum of two numbers 🦁🦁🦁
   
-function add(/*Your Code Here */){
-    /*Your Code Here*/
-  }
+ function add(a,b){ 
+    return a + b;
+}
+console.log(add(3,5));
 
 // 🦁🦁🦁 Use multiply to return the product of two numbers 🦁🦁🦁
   
-function multiply(/*Your Code Here */){
-   /*Your Code Here */
-  }
+function multiply(a,b){
+    return a * b;
+}
+console.log(multiply(3,5,));
 
  // 🦁🦁🦁 Use greeting to accept a first and last name and return "Hello {first-name} {last-name}, nice to meet you!" 🦁🦁🦁
   
-function greeting(/*Your Code Here */){
-   return /*Your Code Here */
-  }
+ function greeting(firstName, lastName,){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+ }
+ console.log(greeting("Yonathan","Admasu"));
   
   // 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
   // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
