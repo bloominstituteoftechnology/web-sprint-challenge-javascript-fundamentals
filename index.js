0faaa3e00c3556368e1299259d63468754848529
Nicole Pizzano <nicolepizzano@Nicoles-iMac.local>
2021-02-21 20:51:47 -0500

    codegrade test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const external = "I'm outside the function";
 
 function myFunction() {
   console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+  const internal = "Hello! I'm inside myFunction!"
 
   function nestedFunction() {
     console.log(internal);
