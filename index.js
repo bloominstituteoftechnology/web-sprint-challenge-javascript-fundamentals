17befc34e7cb53bc1bcc7ebbeeeb94983d2c13b8
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-03-21 17:34:05 -0500

    finished task 2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+//because both nested function and internal are both inside the myFunction function
 
 
 
@@ -28,11 +28,14 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(number) {
+  let counter = 0;
+  for(let i = 0; i < number; i++){
+    counter += i + 1
   }
- 
+  return counter
+ }
+ console.log(summation(4))
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
