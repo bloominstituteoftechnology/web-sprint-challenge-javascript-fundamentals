e8ddd6786145fecfaca1bdd8884f594c68500d4b
Amlak befekadu <amlakbarkon@gmail.com>
2021-04-17 06:19:32 -0400

    done MVP

--- index.js
+++ index.js
@@ -228,7 +228,21 @@ const cuboidtwo = new CuboidMakerTwo({length:4, width:5, height:5});
 
 
 // 🦄 💪 Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area. 🦄 💪
-  
+  class CubeMaker extends CuboidMaker{
+    constructor(length,width,height){
+      super(length,width,height);
+      this.length = length;
+      this.width = width;
+      this.height = height;
+    }
+    volume(){
+      return this.length * this.width * this.height;
+    }
+    surface(){
+
+    }
+    
+  }
 
 
 
