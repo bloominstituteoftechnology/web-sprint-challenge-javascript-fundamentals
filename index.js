2447b58af4017d980e26626cf7e70e286426be6f
scoutreilly <scouty33@gmail.com>
2021-04-18 16:02:52 -0600

    task 1

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
-
+//Because both the nested function and the internal variable are inside the function myFunction. Both are in scope inside myFunction
 
 
 /* 🚀🚀🚀 Task 2: Counter 🚀🚀🚀 */
@@ -28,11 +27,15 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(howMany) {
+  let sum = 0;
+  for (let i = 0; i <= howMany; i++) {
+    sum = sum + i;
   }
- 
+  console.log(sum);
+  return sum
+}
+summation(4)
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
