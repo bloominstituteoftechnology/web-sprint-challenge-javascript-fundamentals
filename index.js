5db6ac885d63a522b11b87e658dc8013e06d5c0d
Brit Hemming <hello@brittanyhemming.com>
2021-07-27 15:10:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -190,11 +190,7 @@ class CuboidMakerTwo{
 
 
 
-// 🦄 💪 Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area. 🦄 💪
   
-
-
-
 
 
   /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
