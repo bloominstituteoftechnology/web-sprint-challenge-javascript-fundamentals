b8ef0518829d6be67ba1a68e871b287fba536d80
Enas <enaselmahdwi@gmail.com>
2021-09-17 10:44:59 -0600

    done with my questions

--- README.md
+++ README.md
@@ -25,17 +25,30 @@ Demonstrate your understanding of this week's concepts by answering the followin
 Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read.
 
 1. Explain the differences between `.map`, `.reduce` and `.filter` and describe a use case for each. 
+.map and .filter both return a new array of element but .reduce return a single value.
+.map anf .filter callback each element,index and return in turn in .reduce function take a previous value and a next value.
+.filter takes a callback that runs a truth test(if true will return the element else ignore) also uses for filtering out an array of element by specific condition.
+.map uses for manipulating or reshaping data.
+.reduce uses for manipulating or reshaping data into a single value.
 
 2. Explain the difference between a callback and a higher order function.
+   A  callback function is passed into other function as argument.
+   A  higher order function(HOF) receives other function as parameters.
 
 3. Explain what a closure is.
-
+  A closure is when an inner function reaches out of its scope to grab a variable defined in an outer function.
+  
 4. Describe the four principles of the 'this' keyword.
-
+    1. Window binding -if we haven't given 'this' any context it will return the window, the global object in node or undefined in strict mode.
+    2. Implicit binding - Applies to objects with methods. When the function (method) is invoked, look the the left of the dot, that's what 'this' refers to.
+    3. Explicit binding - we tell a function what the 'this' keyword should be using .call, .apply or .bind. Call will immediately invoke the function and you pass in your arguments 1 by 1. Apply will immediately invoke the function and you pass in your arguments as an array. Bind you pass in your arguments 1 by 1, but it will not immediately invoke the function, instead it returns a brand new function that can be invoked later.
+    4. New binding - When a function is invoked with a new keyword the this keyword inside that function is bound to the new object being constructed. When a function is invoked as a constructor function using the new keyword, this points to the new object that’s created
 5. Why do we need super() in an extended class?
+    The extends will inherit parent properties and will abstract away call() function.
+    The super()function will inherit parent method to child and  will not ever need it to bind. Also abstract away any of syntax that we have using to being our objectS prototype.
 
 You are expected to be able to answer questions in these areas. Your responses contribute to your Sprint Challenge grade. 
-
+ 
 ## Instructions
 
 ### Task 1: Set up Project
--- index.js
+++ index.js
@@ -17,7 +17,8 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+//TASK1 IS HERE====>Because nestedFunction created in  myFunction so nestedFunction will only ever be avaliable inside of it's parent"myFunction" and also as we know When a function is defined in another function,
+ //the inner function has access to the outer function’s variables.This behavior is called lexical scoping..
 
 
 
@@ -28,9 +29,12 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(num) {
+ let sum=0;
+  for(let i=num;i>=1;i--){
+     sum=i+sum;
+  }
+return sum;
   }
  
 
@@ -56,9 +60,16 @@ const zooAnimals = [
   displayNames will be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+  function animalNames(array){
+    const displayNames = [];
+  array.forEach((element) => {
+    displayNames.push(
+      `name: ${element.animal_name}, scientific: ${element.scientific_name}`
+    );
+  });
+  return displayNames;
+}
+console.log(animalNames(zooAnimals));
   
 
   /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
@@ -67,8 +78,11 @@ const zooAnimals = [
   For example: ['jackal, asiatic', .....]
   */
 
-  function lowerCaseNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowerCaseNames(array){
+    const newArray = array.map((element) => {
+      return element.animal_name.toLowerCase();
+    });
+    return newArray;
   }
   
   
@@ -77,8 +91,9 @@ const zooAnimals = [
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowPopulationAnimals(array){
+    const newArray=array.filter(element =>  element.population<5);
+    return newArray;
   }
   
 
@@ -88,8 +103,9 @@ const zooAnimals = [
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
+  function USApop(array){
+   const total = array.reduce((total1,curentValue) =>{return total1+curentValue.population},0 );
+   return total;
   }
   
   
@@ -101,53 +117,66 @@ const zooAnimals = [
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
 
-  function consume(/*Your Code Here */){
-    /*Your Code Here */
+  function consume(a,b,cb){
+    return cb(a,b);
+
   }
  
   
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Use add to return the sum of two numbers 🦁🦁🦁
   
-function add(/*Your Code Here */){
-    /*Your Code Here*/
+function add(num1,num2,sum){
+    return sum(num1,num2);
+    function sum (number1,number2){ 
+    return number1+number2;
+    }
   }
 
 
 // 🦁🦁🦁 Use multiply to return the product of two numbers 🦁🦁🦁
   
-function multiply(/*Your Code Here */){
-   /*Your Code Here */
+function multiply(num1,num2,multiply){
+  return multiply(num1,num2);
+  function multiply(number1,number2){
+    return number1*number2;
+  }
   }
 
 
  // 🦁🦁🦁 Use greeting to accept a first and last name and return "Hello {first-name} {last-name}, nice to meet you!" 🦁🦁🦁
   
-function greeting(/*Your Code Here */){
-   return /*Your Code Here */
+function greeting(firstName,lastName){
+   return `Hello ${firstName} ${lastName}, nice to meet you!`
   }
   
   
   // 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
   // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
-  // console.log(consume(2, 2, add)); // 4
-  // console.log(consume(10, 16, multiply)); // 160
-  // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+   console.log(consume(2, 2, add)); // 4
+   console.log(consume(10, 16, multiply)); // 160
+   console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 // 🐴🐴🐴 Topic 3: Prototypes 🐴🐴🐴 //
 //🐴🐴🐴 Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge. 🐴🐴🐴
 /* 🐴🐴🐴 Step 1: Base Constructor 🐴🐴🐴
  Use the constructor function named CuboidMaker to accept properties for length, width, and height which can be initialized as an object
 */
-function CuboidMaker(/*Your Code Here */){
-  /*Your Code Here */
-}
+function CuboidMaker(sizeCubes){
+  this.length=sizeCubes.length;
+  this.width=sizeCubes.width;
+  this.height=sizeCubes.height;
 
+}
+const sizeCubes = {length:12,width:5,height:10};
 
 /* 🐴🐴🐴 Step 2: Volume Method 🐴🐴🐴
   Create a method called volume using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   Formula for cuboid volume: length * width * height   */
-
+  CuboidMaker.prototype.volume=function(){
+    const vol=this.length * this.width * this.height
+    return vol;
+  }
 
 
 
@@ -158,30 +187,44 @@ function CuboidMaker(/*Your Code Here */){
   2 * (length * width + length * height + width * height)  */
 
 
-
-
+  CuboidMaker.prototype.surfaceArea=function(){
+   const area=(this.length * this.width + this.length * this.height + this.width * this.height)*2;
+   return area;
+  }
+  
 
 /* 🐴🐴🐴 Step 4: Create a new object that uses CuboidMaker 🐴🐴🐴
   Create an object called cuboid that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid. */
-
-
-
+  const cuboid = new CuboidMaker({length:4,width:5,height:5}); 
+ 
 
 
 // 🐴🐴🐴 Test your volume and surfaceArea methods by uncommenting the logs below: 🐴🐴🐴
 // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+ console.log(cuboid.volume()); // 100
+ console.log(cuboid.surfaceArea()); // 130
  
 
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
 //🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
 class CuboidMakerTwo{
+  constructor(cuboidTwo){ 
+  this.length=cuboidTwo.length;
+  this.width=cuboidTwo.width;
+  this.height=cuboidTwo.height;
+  }
 
+volume(){
+  const vol=this.length * this.width * this.height
+  return vol;
 }
-
-
+surfaceArea(){
+  const area=(this.length * this.width + this.length * this.height + this.width * this.height)*2;
+  return area;
+ }
+}
+const cuboidTwo = {length:12,width:5,height:10};
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
 // console.log(cuboidTwo.volume()); // 100
 // console.log(cuboidTwo.surfaceArea()); // 130
