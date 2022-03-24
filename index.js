4dd1db3b135a7e54559763c98060e2a4ff0c387a
Samantha Goodman <antashma@gmail.com>
2022-03-24 14:22:47 -0400

    more edits to step 2--forgot to save

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ const zooAnimals = [
   // 🦁🦁🦁 Callbacks 🦁🦁🦁  
   /* 🦁🦁🦁 Step 1: Create a higher-order function 🦁🦁🦁
   Use the higher-order function called consume to do the following:
-    1. Recieve 3 parameters: a, b and cb. The first two parameters (a and b) can take any argument (we can pass any value as an argument) and the last parameter (cb) accepts a callback
+    1. Receive 3 parameters: a, b and cb. The first two parameters (a and b) can take any argument (we can pass any value as an argument) and the last parameter (cb) accepts a callback
     2. Return the invocation of cb taking a and b its arguments
     
     💡 NOTE: The tests for 'consume' will pass if it is created corrently and also after you correctly complete the functions 'add' and 'greeting' below in Step 2.
@@ -113,7 +113,7 @@ const zooAnimals = [
   // 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁
 
  /*  Use add to do the following:
- 1. Recieve two numbers as an argument that are passed in from its first and second parameters
+ 1. Receive two numbers as an argument that are passed in from its first and second parameters
  2. Return the sum of those numbers
  */
 
@@ -123,7 +123,7 @@ function add(/*Your Code Here */){
 
 
 /* Use multiply to do the following:
-1. Recieve two numbers as an argument that are passed in from its first and second parameters
+1. Receive two numbers as an argument that are passed in from its first and second parameters
 2. Return the product of those numbers
 */
 
@@ -133,7 +133,7 @@ function multiply(/*Your Code Here */){
 
 
  /* Use greeting to do the following:
-1. Recieve two strings (a first name and last name) as an argument that are passed in from its first and second parameters
+1. Receive two strings (a first name and last name) as an argument that are passed in from its first and second parameters
 2. Return "Hello {first-name} {last-name}, nice to meet you!"
 💡 NOTE: The string returned must match the format above or the test will not pass!
 */
@@ -143,12 +143,11 @@ function greeting(/*Your Code Here */){
   }
   
   
-
-  // 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
-  // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
-  // console.log(consume(2, 2, add)); // 4
-  // console.log(consume(10, 16, multiply)); // 160
-  // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+// 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
+// ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
+// console.log(consume(2, 2, add)); // 4
+// console.log(consume(10, 16, multiply)); // 160
+// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 // 🐴🐴🐴 Topic 3: Prototypes 🐴🐴🐴 //
 //🐴🐴🐴 Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge. 🐴🐴🐴
